77447cb9afda1e7a4dcdec88880dd9e9d50921ee
ash <ashyantel@gmail.com>
2025-04-07 20:30:59 -0400

    initial commit

---
+++ src/ListeDoublementChaine.java
@@ -0,0 +1,412 @@
+
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.*;
+import java.util.stream.*;
+
+/**
+ * Implementation d'un type de donnees abstrait de {\code ListeDoublementChaine} a l'aide d'un double chainage.
+ *
+ * Les positions dans la {\code ListeDoublementChaine} sont designees par des entiers.  L'element a la premiere
+ * case de la liste est a la position {\code 0} et l'element a la derniere case est a la position
+ * {\code taille() - 1}.
+ * @param <E> Le type des elements places dans la {\code ListeDoublementChaine}.
+ */
+public class ListeDoublementChaine< E >
+        implements Iterable< E > {
+    private static final int POSITION_MINIMUM_INFERIEUR = 0;
+    /**
+     * Une classe interne pour representer un {\code Chainon} contenant un element.
+     * Il contient aussi deux references.  Une vers le {\code Chainon} precedant et une autre
+     * vers le {\code Chainon} suivant.
+     */
+    private static class Chainon< E > {
+        public E element;
+        public Chainon< E > precedant;
+        public Chainon< E > suivant;
+
+        public Chainon( Chainon<E > precedant, E element ) {
+            this.element = element;
+            this.precedant = precedant;
+        }
+
+        public Chainon( Chainon< E > precedant, E element, Chainon< E > suivant ) {
+            this.element = element;
+            this.precedant = precedant;
+            this.suivant = suivant;
+        }
+    }
+
+    private static class ListeIterator< E > implements Iterator< E > {
+
+        private Chainon< E > courant;
+
+        public ListeIterator( Chainon< E > tete ) {
+            courant = tete;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return null != courant;
+        }
+
+        @Override
+        public E next() {
+            E element = courant.element;
+            courant = courant.suivant;
+            return element;
+        }
+
+    }
+
+    static private class ListeSpliterator< E > implements Spliterator< E > {
+        private static final int CHARACTERISTIQUES = ORDERED | SIZED | SUBSIZED;
+        private static final int TAILLE_MINIMAL_SPLIT = 5;
+
+        private int debut;
+        private int fin;
+        private Chainon< E > courant;
+
+        public ListeSpliterator( int debut, int fin, Chainon< E > courant ) {
+            this.debut = debut;
+            this.fin = fin;
+            this.courant = courant;
+        }
+
+        @Override
+        public boolean tryAdvance( Consumer< ? super E > action ) {
+            boolean resultat = false;
+
+            if( debut < fin ) {
+                resultat = true;
+                action.accept( courant.element );
+                courant = courant.suivant;
+                ++ debut;
+            }
+
+            return resultat;
+        }
+
+        @Override
+        public Spliterator< E > trySplit() {
+            ListeSpliterator< E > resultat = null;
+
+            if( TAILLE_MINIMAL_SPLIT <= ( debut - fin ) ) {
+                int milieu = ( debut + fin ) / 2;
+                resultat = new ListeSpliterator<>( debut, milieu, courant );
+                for( int i = debut; i <= milieu; ++ i ) {
+                    courant = courant.suivant;
+                }
+                debut = milieu + 1;
+            }
+
+            return resultat;
+        }
+
+        @Override
+        public long estimateSize() {
+            return fin - debut;
+        }
+
+        @Override
+        public int characteristics() {
+            return CHARACTERISTIQUES;
+        }
+    }
+
+    /**
+     * methode interne utilisee pour retrouver un {\code Chainon} selon sa position.
+     * @param position La position du {\code Chainon}.  Le premier {\code Chainon}
+     *                 est a la position {\code 0}.
+     * @return Le {\code Chainon} demande.
+     * @throws IndexOutOfBoundsException Lance si la position est invalide : {\code position < 0 || taille() <= position}.
+     */
+    private Chainon< E > getChainon( int position )
+            throws IndexOutOfBoundsException {
+        if( position < POSITION_MINIMUM_INFERIEUR || taille <= position  ) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Chainon< E > courant = tete;
+
+        while( position != 0 ) {
+            courant = courant.suivant;
+            -- position;
+        }
+
+        return courant;
+    }
+
+
+    /**
+     * Une reference au premier element de la {\code ListeDoublementChaine} est maintenu ici.
+     */
+    protected Chainon< E > tete;
+
+    /**
+     * Puisque l'ajout a la fin d'une {\code ListeDoublementChaine} est courante, un reference est
+     * conserve pour obtenir rapidement le dernier element.
+     */
+    protected Chainon< E > fin;
+
+    protected int taille;
+
+
+    /**
+     * Construit une {\code ListeDoublementChaine} vide.
+     */
+    public ListeDoublementChaine() {
+        tete = null;
+        taille = 0;
+    }
+
+    /**
+     * Construit une {\code ListeDoublementChaine} à l'aide d'une {\code Collection} d'éléments.
+     */
+    public ListeDoublementChaine( Collection< E > elements ) {
+        this();
+
+        for( E element : elements ) {
+            inserer( element );
+        }
+    }
+
+
+    @Override
+    public boolean equals( Object droite ) {
+        return switch( droite ) {
+            case ListeDoublementChaine temp -> {
+
+                Iterator< E > itGauche = this.iterator();
+                Iterator< E > itDroite = temp.iterator();
+
+                while( itGauche.hasNext() && itDroite.hasNext()
+                       && itGauche.next().equals( itDroite.next() ) );
+
+                yield ! ( itGauche.hasNext() || itDroite.hasNext() );
+            }
+            default -> false;
+        };
+    }
+
+
+    /**
+     * Donne l'element a la position indiquee.
+     * @param position La position de l'element a extraire.
+     * @return L'element extrait.
+     * @throws IndexOutOfBoundsException Lance si la position est invalide : {\code position < 0 || taille() <= position}.
+     */
+    public E get( int position )
+            throws IndexOutOfBoundsException {
+        return getChainon( position ).element;
+    }
+
+
+    /**
+     * Ajoute un element a la fin de la {\code ListeDoublementChaine}
+     * @param element L'element a ajouter.
+     */
+    public void inserer( E element ) {
+        Chainon< E > nouveau = new Chainon<>( fin, element );
+
+        if( taille == 0 ) {
+            tete = nouveau;
+        } else {
+            fin.suivant = nouveau;
+        }
+
+        fin = nouveau;
+
+        ++ taille;
+    }
+
+
+    /**
+     * Ajoute un element dans la {\code ListeDoublementChaine} a la position indiquee.
+     * @param position La position ou l'element est ajoute.  Les valeurs au position suivante seront
+     *                 decalees de une case.  Un ajout a la position {\code taille()} ajoute l'element a la
+     *                 fin de la {\code ListeDoublementChaine}.
+     * @param element L'element a ajouter.
+     * @throws IndexOutOfBoundsException Lance si la position est invalide : {\code position < 0 || taille() < position}.
+     */
+    public void inserer( int position, E element )
+            throws IndexOutOfBoundsException {
+        if( position == taille ) {
+            inserer( element );
+        } else {
+            Chainon< E > courant = getChainon( position );
+
+            Chainon< E > nouveau = new Chainon<>( courant.precedant, element, courant );
+
+            if( courant.precedant == null ) {
+                tete = nouveau;
+            } else {
+                courant.precedant.suivant = nouveau;
+            }
+            courant.precedant = nouveau;
+
+            ++ taille;
+        }
+    }
+
+
+    /**
+     * genere un {\code Iterator}
+     * @return Un {\code Iterator}
+     */
+    @Override
+    public Iterator< E > iterator() {
+        return new ListeIterator<>( tete );
+    }
+
+
+    /**
+     * Construit un {\code Stream} pour la {\code Liste}.
+     *
+     * IMPORTANT : ce Stream n'est pas protégé contre les effets qui modifient la Liste lors
+     * de l'activation du Stream.
+     * @return Un Stream sur la liste
+     */
+    public Stream< E > stream() {
+        return StreamSupport.stream( () -> new ListeSpliterator<>( 0, taille, tete ),
+                ListeSpliterator.CHARACTERISTIQUES, false );
+    }
+
+
+    /**
+     * Construit un {\code Stream} pour la {\code Liste}.
+     *
+     * IMPORTANT : ce Stream n'est pas protégé contre les effets qui modifient la Liste lors
+     * de l'activation du Stream.
+     * @return Un Stream sur la liste
+     */
+    public Stream< E > parallelStream() {
+        return StreamSupport.stream( () -> new ListeSpliterator<>( 0, taille, tete ),
+                ListeSpliterator.CHARACTERISTIQUES, true );
+    }
+
+
+    /**
+     * Modifie l'element a la position indiquee.
+     * @param position La position de l'element a modifier.
+     * @param element L'element qui va remplacer l'ancien element.
+     * @throws IndexOutOfBoundsException Lance si la position est invalide : {\code position < 0 || taille() <= position}.
+     */
+    public void set( int position, E element )
+            throws IndexOutOfBoundsException {
+        getChainon( position ).element = element;
+    }
+
+
+    /**
+     * Enleve un element de la {\code ListeDoublementChaine}
+     * @param position La position de l'element a supprimer.
+     * @throws IndexOutOfBoundsException Lance si la position est invalide : {\code position < 0 || taille() <= position}.
+     */
+    public void supprimer( int position )
+            throws IndexOutOfBoundsException {
+        Chainon< E > courant = getChainon( position );
+
+        if( null == courant.precedant ) {
+            tete = courant.suivant;
+        } else {
+            courant.precedant.suivant = courant.suivant;
+        }
+
+        if( null == courant.suivant ) {
+            fin = courant.precedant;
+        } else {
+            courant.suivant.precedant = courant.precedant;
+        }
+
+        -- taille;
+    }
+
+
+    /**
+     * La taille de la {\code ListeDoublementChaine}.
+     * @return Le nombre d'element que contient la {\code ListeDoublementChaine}.
+     */
+    public int taille() {
+        return taille;
+    }
+
+
+    @Override
+    public String toString() {
+        return stream()
+                .map( ( e ) -> e.toString() )
+                .collect( Collectors.joining( ", ", "[ ", " ]" ) );
+    }
+
+
+    /***************************************************************/
+    /***************************************************************/
+    /***************************************************************/
+
+
+    /**
+     * Construit une nouvelle liste avec les éléments de la liste originale (this)
+     * qui donnent True pour le prédicat.
+     * La nouvelle liste est retournée par la méthode.
+     * Aussi, les éléments qui ont donnés True sont enlevé de la liste originale.
+     * @param condition Le prédicat qu'un élément doit respecter pour être accepté dans la nouvelle liste.
+     *                  Le comportement de la méthode n'est pas définie si ce paramètre est {\code null}.
+     * @return La liste des éléments qui ont donnés True.
+     */
+    public ListeDoublementChaine< E > extraireSi( Predicate< E > condition ) {
+        return null;
+    }
+
+
+    /**
+     * Vérifie si les éléments de la liste originale (this) sont présent
+     * dans la liste en paramètre.
+     * @param droite La liste dans laquelle la méthode cherche les éléments de la liste
+     *               originale.  Le comportement de la méthode n'est par définie si ce
+     *               paramètre est {\code null}.
+     * @return True si tous les éléments de la liste orginale sont présents dans la liste
+     * en paramètre.  False sinon.
+     */
+    public boolean sousEnsembleEgal( ListeDoublementChaine< E > droite ) {
+        return false;
+    }
+
+
+    /**
+     * Vérifie que les éléments d'une liste sont placés de tel sorte que les éléments qui
+     * donne True selon le prédicat viennent tous avant les éléments qui donnes False.
+     * <p>
+     * Cas spéciaux :
+     * <ul>
+     *     <li>Une liste vide donne toujours True.</li>
+     *     <li>Si tous les éléments de la liste donne True, alors le résultat est True.</li>
+     *     <li>Si tous les éléments de la liste donne False, alors le résultat est True.</li>
+     * </ul>
+     * </p>
+     * @param condition Le prédicat utilisé pour vérifier si la liste est divisée correctement.
+     *                  Le comportement de la méthode n'est pas défini si ce paramètre est {\code null}.
+     * @return True si la liste est divisé selon la condition en paramètre.
+     */
+    public boolean estDiviseSelon( Predicate< E > condition ) {
+        return false;
+    }
+
+
+    /**
+     * Déplace les éléments de la liste pour que la méthode {\code estDiviseSelon} puisse donner True.
+     * Donc, après l'appel de cette méthode, les éléments qui donnent True avec le prédicat doivent
+     * venir avant tous les éléments qui donnent False.
+     * L'ordre relatif des éléments qui donnent True doit être conservé.
+     * C'est à dire que pour toutes paire de deux éléments au position i et j respectivement,
+     * si le prédicat donne True pour les deux éléments et que i < j avant le remaniment de la liste
+     * alors i < j aussi après le remaniment de la liste.
+     * Même chose pour les éléments qui donnent False, leurs ordre relatif doit être conservé.
+     * @param condition Le prédicat utiliser pour diviser la liste.
+     *                  Le comportement de la méthode n'est pas défini si ce paramètre est {\code null}.
+     */
+    public void diviserSelon( Predicate< E > condition ) {
+    }
+}
---
+++ src/ListeDoublementChaineTest.java
@@ -0,0 +1,387 @@
+package structure;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListeDoublementChaineTest {
+
+    @Test
+    void extraireSi1() {
+        ListeDoublementChaine< Integer > listeAvant =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9 )
+                );
+        ListeDoublementChaine< Integer > listeApres =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 5, 7, 9 )
+                );
+        ListeDoublementChaine< Integer > listeAttendu =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 2, 4, 6, 8 )
+                );
+
+        assertEquals( listeAttendu, listeAvant.extraireSi( ( x ) -> ( x % 2 ) == 0 ) );
+        assertEquals( listeApres, listeAvant );
+    }
+
+    @Test
+    void extraireSi2() {
+        ListeDoublementChaine< Integer > listeAvant =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 5, 7, 9 )
+                );
+        ListeDoublementChaine< Integer > listeApres =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 5, 7, 9 )
+                );
+        ListeDoublementChaine< Integer > listeAttendu =
+                new ListeDoublementChaine<>();
+
+        assertEquals( listeAttendu, listeAvant.extraireSi( ( x ) -> ( x % 2 ) == 0 ) );
+        assertEquals( listeApres, listeAvant );
+    }
+
+    @Test
+    void extraireSi3() {
+        ListeDoublementChaine< Integer > listeAvant =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, -2, -3, 4, 5, -6, -7, 8, 9 )
+                );
+        ListeDoublementChaine< Integer > listeApres =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 4, 5, 8, 9 )
+                );
+        ListeDoublementChaine< Integer > listeAttendu =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( -2, -3, -6, -7 )
+                );
+
+        assertEquals( listeAttendu, listeAvant.extraireSi( ( x ) -> x < 0 ) );
+        assertEquals( listeApres, listeAvant );
+    }
+
+    @Test
+    void extraireSi4() {
+        ListeDoublementChaine< Integer > listeAvant =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( -2, -3, -6, -7 )
+                );
+        ListeDoublementChaine< Integer > listeApres =
+                new ListeDoublementChaine<>(
+                );
+        ListeDoublementChaine< Integer > listeAttendu =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( -2, -3, -6, -7 )
+                );
+
+        assertEquals( listeAttendu, listeAvant.extraireSi( ( x ) -> x < 0 ) );
+        assertEquals( listeApres, listeAvant );
+    }
+
+    @Test
+    void extraireSi5() {
+        ListeDoublementChaine< Integer > listeAvant =
+                new ListeDoublementChaine<>();
+        ListeDoublementChaine< Integer > listeApres =
+                new ListeDoublementChaine<>();
+        ListeDoublementChaine< Integer > listeAttendu =
+                new ListeDoublementChaine<>();
+
+        assertEquals( listeAttendu, listeAvant.extraireSi( ( x ) -> x < 0 ) );
+        assertEquals( listeApres, listeAvant );
+    }
+
+    @Test
+    void sousEnsembleEgal1() {
+        ListeDoublementChaine< Integer > listeSous =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 5, 7, 9 )
+                );
+        ListeDoublementChaine< Integer > listeSousCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 5, 7, 9 )
+                );
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9 )
+                );
+        ListeDoublementChaine< Integer > listeCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9 )
+                );
+
+        assertTrue( listeSous.sousEnsembleEgal( liste ) );
+        assertFalse( liste.sousEnsembleEgal( listeSous ) );
+        assertEquals( listeSous, listeSousCopie );
+        assertEquals( liste, listeCopie );
+    }
+
+    @Test
+    void sousEnsembleEgal2() {
+        ListeDoublementChaine< Integer > listeSous =
+                new ListeDoublementChaine<>();
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9 )
+                );
+        ListeDoublementChaine< Integer > listeCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9 )
+                );
+
+        assertTrue( listeSous.sousEnsembleEgal( liste ) );
+        assertFalse( liste.sousEnsembleEgal( listeSous ) );
+        assertEquals( 0, listeSous.taille() );
+        assertEquals( liste, listeCopie );
+    }
+
+    @Test
+    void sousEnsembleEgal3() {
+        ListeDoublementChaine< Integer > listeSous =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5 )
+                );
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5 )
+                );
+
+        assertTrue( listeSous.sousEnsembleEgal( liste ) );
+        assertTrue( liste.sousEnsembleEgal( listeSous ) );
+    }
+
+    @Test
+    void sousEnsembleEgal4() {
+        ListeDoublementChaine< Integer > listeSous =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 1, 2, 2, 2, 2, 2 )
+                );
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5 )
+                );
+
+        assertTrue( listeSous.sousEnsembleEgal( liste ) );
+        assertFalse( liste.sousEnsembleEgal( listeSous ) );
+    }
+
+    @Test
+    void sousEnsembleEgal5() {
+        ListeDoublementChaine< Integer > listeSous =
+                new ListeDoublementChaine<>();
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>();
+
+        assertTrue( listeSous.sousEnsembleEgal( liste ) );
+    }
+
+    @Test
+    void estDiviseSelon1() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 1, 3, 5, 7 )
+                );
+        ListeDoublementChaine< Integer > listeCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 1, 3, 5, 7 )
+                );
+
+        assertTrue( liste.estDiviseSelon( ( e ) -> e < 2 ) );
+        assertEquals( liste, listeCopie );
+    }
+
+    @Test
+    void estDiviseSelon2() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 3, 5, 1 )
+                );
+        ListeDoublementChaine< Integer > listeCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 3, 5, 1 )
+                );
+
+        assertFalse( liste.estDiviseSelon( ( e ) -> e < 2 ) );
+        assertEquals( liste, listeCopie );
+    }
+
+    @Test
+    void estDiviseSelon3() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 2 )
+                );
+        ListeDoublementChaine< Integer > listeCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 2 )
+                );
+
+        assertTrue( liste.estDiviseSelon( ( e ) -> ( e % 2 ) == 0 ) );
+        assertEquals( liste, listeCopie );
+    }
+
+    @Test
+    void estDiviseSelon4() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>();
+
+        assertTrue( liste.estDiviseSelon( ( e ) -> ( e % 2 ) == 0 ) );
+        assertEquals( 0, liste.taille() );
+    }
+
+    @Test
+    void estDiviseSelon5() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 3, 5, 1 )
+                );
+        ListeDoublementChaine< Integer > listeCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 3, 5, 1 )
+                );
+
+        assertTrue( liste.estDiviseSelon( ( e ) -> 0 < e ) );
+        assertEquals( liste, listeCopie );
+    }
+
+    @Test
+    void estDiviseSelon6() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 3, 5, 1 )
+                );
+        ListeDoublementChaine< Integer > listeCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 3, 3, 5, 1 )
+                );
+
+        assertTrue( liste.estDiviseSelon( ( e ) -> ( e % 2 ) == 0 ) );
+        assertEquals( liste, listeCopie );
+    }
+
+    @Test
+    void estDiviseSelon7() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1 )
+                );
+        ListeDoublementChaine< Integer > listeCopie =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1 )
+                );
+
+        assertTrue( liste.estDiviseSelon( ( e ) -> e != 1 ) );
+        assertEquals( liste, listeCopie );
+    }
+
+    @Test
+    void diviserSelon1() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9 )
+                );
+        ListeDoublementChaine< Integer > listeResultat =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 2, 4, 6, 8, 1, 3, 5, 7, 9 )
+                );
+
+        liste.diviserSelon( ( e ) -> ( e % 2 ) == 0 );
+
+        assertEquals( listeResultat, liste );
+    }
+
+    @Test
+    void diviserSelon2() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>();
+        ListeDoublementChaine< Integer > listeResultat =
+                new ListeDoublementChaine<>();
+
+        liste.diviserSelon( ( e ) -> ( e % 2 ) == 0 );
+
+        assertEquals( listeResultat, liste );
+    }
+
+    @Test
+    void diviserSelon3() {
+        ListeDoublementChaine< Integer > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1 )
+                );
+        ListeDoublementChaine< Integer > listeResultat =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( 1 )
+                );
+
+        liste.diviserSelon( ( e ) -> e < 100 );
+
+        assertEquals( listeResultat, liste );
+    }
+
+    @Test
+    void diviserSelon4() {
+        ListeDoublementChaine< String > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( "allo", "a" )
+                );
+        ListeDoublementChaine< String > listeResultat =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( "a", "allo" )
+                );
+
+        liste.diviserSelon( ( e ) -> e.length() == 1 );
+
+        assertEquals( listeResultat, liste );
+    }
+
+    @Test
+    void diviserSelon5() {
+        ListeDoublementChaine< String > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( "a", "n", "du", "e" )
+                );
+        ListeDoublementChaine< String > listeResultat =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( "a", "n", "e", "du" )
+                );
+
+        liste.diviserSelon( ( e ) -> e.length() == 1 );
+
+        assertEquals( listeResultat, liste );
+    }
+
+    @Test
+    void diviserSelon6() {
+        ListeDoublementChaine< String > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( "comment", "ni", "devra", "etre" )
+                );
+        ListeDoublementChaine< String > listeResultat =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( "etre", "comment", "ni", "devra" )
+                );
+
+        liste.diviserSelon( ( e ) -> e.length() == 4 );
+
+        assertEquals( listeResultat, liste );
+    }
+
+    @Test
+    void diviserSelon7() {
+        ListeDoublementChaine< String > liste =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( "etre", "comment", "ni", "devra" )
+                );
+        ListeDoublementChaine< String > listeResultat =
+                new ListeDoublementChaine<>(
+                        Arrays.asList( "comment", "ni", "devra", "etre" )
+                );
+
+        liste.diviserSelon( ( e ) -> e.length() != 4 );
+
+        assertEquals( listeResultat, liste );
+    }
+}
